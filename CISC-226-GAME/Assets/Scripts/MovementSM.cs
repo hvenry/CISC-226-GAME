b40a9d7340943cde902581343aa484d823ff41c7
hvenry <hvendittelli@gmail.com>
2023-03-02 16:17:19 -0500

    Added comments to MovementSM and Idle

--- CISC-226-GAME/Assets/Scripts/MovementSM.cs
+++ CISC-226-GAME/Assets/Scripts/MovementSM.cs
@@ -4,24 +4,23 @@ using UnityEngine;
 
 public class MovementSM : StateMachine
 {
-    [HideInInspector]
+    // Access all our animal states (scripts) inside one script
     public Idle idleState;
-    [HideInInspector]
     public Roaming roamState;
-    [HideInInspector]
     public Fleeing fleeState;
-    [HideInInspector]
     public Held heldState;
-    [HideInInspector]
     public Thrown thrownState;
-    
-    
-    [HideInInspector]
     public Rigidbody2D rigidbody;
+
+    // Hide HideInInspector makes the variables only accessable in the script
+    // [HideInInspector]
     public float speed = 2f;
 
+    // Awake always gets called the first time a game object is created in a scene
     private void Awake()
     {
+        // For our movement, we will need to access all animal states inside one script
+        // and add them to the animal's state machine
         idleState = new Idle(this);
         roamState = new Roaming(this);
         fleeState = new Fleeing(this);
@@ -29,6 +28,8 @@ public class MovementSM : StateMachine
         thrownState = new Thrown(this);
         rigidbody = GetComponent<Rigidbody2D>();
     }
+
+    // Override the GetInitialState method to return the initial state of the animal (idleState)
     protected override BaseState GetInitialState()
     {
         return idleState;
