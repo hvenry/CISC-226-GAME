527f1d58d71af01b54068598816d8f927c944dbb
BarkevK <81187998+BarkevK@users.noreply.github.com>
2023-03-14 18:42:07 -0400

    tweak to pen collider script

--- CISC-226-GAME/Assets/Scripts/PenCollision.cs
+++ CISC-226-GAME/Assets/Scripts/PenCollision.cs
@@ -6,12 +6,13 @@ using System.Collections;
 public class PenCollision : MonoBehaviour
 {
     
-    public string TagToIgnore = "Pig";
+    public string TagToIgnore = "";
      
     void OnCollisionEnter2D(Collision2D collision){
         if(collision.gameObject.tag == TagToIgnore ){
             Physics2D.IgnoreCollision( collision.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
         }
+        
     } 
 
 }
