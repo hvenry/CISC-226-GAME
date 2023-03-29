b8e8fea159a425cd035f38019be32c13a7afff72
BarkevK <19bks5@queensu.ca>
2023-03-29 11:15:22 -0400

    Asset changes

---
+++ CISC-226-GAME/Assets/SuperTiled2Unity/Imports/blockPack_tilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="blockPack_tilesheet" tilewidth="64" tileheight="64" tilecount="140" columns="20">
+ <image source="blockPack_tilesheet.png" width="1280" height="500"/>
+</tileset>
---
+++ CISC-226-GAME/Assets/SuperTiled2Unity/Imports/blockPack_tilesheet@2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="blockPack_tilesheet@2" tilewidth="64" tileheight="64" tilecount="600" columns="40">
+ <image source="blockPack_tilesheet@2.png" width="2560" height="1000"/>
+</tileset>
